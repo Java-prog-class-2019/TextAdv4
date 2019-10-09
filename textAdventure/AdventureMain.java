ef21db84ae19cca96efebd2e54398b782eadcf06
Eben Mclean <eben.mclean42@gmail.com>
2019-10-09 12:20:35 -0400

    To do list
    Added a short to do list for things we should look into and start on

--- textAdventure/AdventureMain.java
+++ textAdventure/AdventureMain.java
@@ -1,5 +1,13 @@
 package textAdventure;
 
+/* TO DO LIST
+ * Create object constructor for rooms (array list or hashmap?)
+ * Create object constructor for items 
+ * create static values for character (exmp: health, inventory size,)
+ * Figure out how to track player progression and movement
+ * figure out how to make a window with the Console
+ * create setup method and main game loop
+ */
 
 public class AdventureMain {
 
