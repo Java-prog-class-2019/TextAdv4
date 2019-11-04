2f86483dce39d5c165fce0d14387edd7ab15a0ca
jadewei33 <56309124+jadewei33@users.noreply.github.com>
2019-11-04 06:43:15 -0500

    Update Item.java

--- textAdventure/Item.java
+++ textAdventure/Item.java
@@ -10,7 +10,6 @@ class Item{
 	int foodPoints;
 	String location;
 
-
 	//constructor. It is private so items can only be made from this class. eg. makeItem().
 	private Item(String itemName, String desc, String location, boolean consumable, boolean edible, int foodPoints, boolean canRead) {	
 		this.itemName = itemName;
@@ -49,15 +48,15 @@ class Item{
 	static void makeItem(ArrayList<Item> items) {
 		items.add(new Item("torch","A flickering torch","Lab 1" , false, false, 0, false));
 		items.add(new Item("hammer", "A small, worn hammer that looks like it could fall apart at any moment","Maintenance Area" , true, false,0,false));
-		//use?items.add(new Item("screwdriver", "A regular flathead screwdriver, it looks pretty worn", "Maintenance Area", false, false,0,false));
+		items.add(new Item("screwdriver", "A regular flathead screwdriver, it looks pretty worn", "Maintenance Area", false, false,0,false));
 		items.add(new Item("bread","A stale piece of bread that as far as you can tell isn't mouldy, yet", "Mess Hall", true, true,0,false));
 		items.add(new Item("keycard","A small plastic card that can be used to open certain doors in the facility", "Armoury", false,false, 0, false));
 		items.add(new Item("kerosene","Using the research facility's equipment, you collected crude oil and distilled it into kerosene", "Dig Site", false, false, 0, false));
 		items.add(new Item("flamethrower","A worn, but still functioning flamethrower. It seems that it can add heat to melt something", "Armoury", false,false, 0, false));
-		items.add(new Item("artifact","An mysterious looking artifact. While it is in your possession, you feel yourself getting weaker", "Secret Room", false,false, 0, false));
+		items.add(new Item("artifact","An mysterious looking artifact. While it is in your possession, you feel yourself getting weaker", "Secret Room", false,false, 0, true));
 		items.add(new Item("researchpaper","You found a research paper. It seems as if the scientists were looking to extract oil from the dig site. Perhaps you could read it", "Lab 2", false,false, 0, true));
 		items.add(new Item("jacket","An old wethered jacket that seems a little worse for ware but keeps you warm when you wear it","Living Quarters", false, false,0, false));
-		items.add(new Item("note","There seems to be some writing on it","Dig Site",false,false,0,false));
+		items.add(new Item("note","There seems to be some writing on it","Dig Site",false,false,0,true));
 		
 		/*//DEBUG
 		for(Item i: items) {
