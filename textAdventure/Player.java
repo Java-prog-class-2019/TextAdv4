824b30d95252c05435ee896ffa92e1d4a74829c8
jadewei33 <56309124+jadewei33@users.noreply.github.com>
2019-11-04 06:43:52 -0500

    Update Player.java

--- textAdventure/Player.java
+++ textAdventure/Player.java
@@ -2,9 +2,6 @@ package textAdventure;
 
 class Player {
 	
-	int health = 100, invSize /*temperature? */; // Health, inventory size
-	
-	 //Can move to main advMain in the parse command loop to count how many commands player has inputted.
-	int cmdCount; 
+	int health = 100, invSize; // Health, inventory size
 	
 }
