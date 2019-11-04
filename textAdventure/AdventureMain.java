37233eb1714e02fe72a9690e497d1a03aad8c2ec
jadewei33 <56309124+jadewei33@users.noreply.github.com>
2019-11-04 06:37:18 -0500

    Update AdventureMain.java

--- textAdventure/AdventureMain.java
+++ textAdventure/AdventureMain.java
@@ -8,26 +8,26 @@ public class AdventureMain {
 		new AdventureMain(); //this means that we don't need to make everything static
 	}
 
+	//Global variables
 	Scanner sc = new Scanner(System.in);
 	HashMap<String,Room> roomList = new HashMap<String, Room>();
 	Room currentRoom; 
 	ArrayList<Item> items = new ArrayList<Item>();
 	ArrayList<Item> invList = new ArrayList<Item>();
 	Player player;
-	String word1, word2, word3;
+	String word1, word2, word3; //Text word split
 	boolean bookshelf = true; // Secret room puzzle
 	boolean boardedUp = true; //Living Quarters puzzle
 	boolean wearingCoat = false; //Jacket puzzle - must be wearing jacket to go outside
 	boolean frozenPipes = true,artifactInPlace = false, needFuel = true; // Helicopter puzzle
 	boolean won = false, dead = false; // win/lose conditions
-	//Put global variables here^^^
 
 	AdventureMain() {
 		setUp();
 		gamemain();
 	}
 
-	//main game Method
+	//Main game Method
 	void gamemain() {
 		////Instance variables
 		boolean playing = true;
@@ -35,13 +35,19 @@ public class AdventureMain {
 
 		/***** MAIN GAME LOOP *****/
 		while (playing) { 
+			
+			//Reads user input
 			command = getCommand(); 
 			playing = parseCommand(command);
+			
+			//Player death
 			if (player.health <= 0) {
 				dead = true;
 				System.out.println("You have died");
 				playing = false;
 			}
+			
+			//Player win
 			if (won) {
 				System.out.println("You have successfully escaped the facility");
 				playing = false;
@@ -52,6 +58,7 @@ public class AdventureMain {
 	}
 
 	void setUp() {
+		//Initial start up of the room and variables
 		Item.makeItem(items); //this will make all items and add them to the items arraylist
 		Room.setupRooms(roomList);
 		currentRoom = roomList.get("Lab1");
@@ -63,7 +70,8 @@ public class AdventureMain {
 		player = new Player();
 	}
 
-	String getCommand() { //gets user input
+	String getCommand() { 
+		//Gets user input
 		System.out.println("Please enter a command:");
 		String text = sc.nextLine();
 		text  = text.trim();
@@ -71,6 +79,7 @@ public class AdventureMain {
 	}
 
 	boolean parseCommand(String text) {
+		//Parses user commands
 		text = text.toLowerCase().trim();	
 		text = text.toUpperCase();
 		String words[] = text.split(" ");
@@ -115,7 +124,7 @@ public class AdventureMain {
 			}
 			break;
 		case "HELP":
-			System.out.println("Here is a list of commands you can use:\nNorth, South, East, West, Up, Down\nEat\nSearch\nInventory\nExit");
+			System.out.println("Here is a list of commands you can use:\nNorth, South, East, West, Up, Down\nSearch\nInventory\nExit");
 			break;
 		case "SEARCH":
 			searchRoom();
@@ -138,13 +147,7 @@ public class AdventureMain {
 						System.out.println("The artifact fit perfectly inside the gap of the instrument panel");	
 					}
 				}
-				if(word2.equals("HAMMER")) {
-					if(searchInv("hammer")) {
-						boardedUp = false;
-						System.out.println("You used the hammer to take down the wood");
-					}
-					else System.out.println("You don't have the required supplies");
-				}
+
 				if(word2.equals("KEROSENE")){
 					if(searchInv("kerosene")) {
 						needFuel = false;
@@ -153,6 +156,16 @@ public class AdventureMain {
 					else System.out.println("You don't have fuel");
 				}
 			}
+			else if(currentRoom.equals(roomList.get("Hall2"))){
+				if(word2.equals("HAMMER")) {
+				if(searchInv("hammer")) {
+					boardedUp = false;
+					System.out.println("You used the hammer to take down the wood");
+				}
+				else System.out.println("You don't have the required supplies");
+				}
+			}
+			else System.out.println("You're not in the right area or cannot use those items in that way");
 			break;
 		case "LEAVE":
 			System.out.println("You awaken your dormant powers of temporal manipulation to travel behind the universal curtain to escape.\n"
@@ -160,20 +173,6 @@ public class AdventureMain {
 					+ "GG I guess?\n");
 			won = true;
 			break;
-			//Use this for testing puzzles while wear and use cannot work
-			/*	case "HAMMER":
-				if(searchInv("hammer")) {
-					boardedUp = false;
-					System.out.println("You used the hammer to take down the wood");
-				}
-				else System.out.println("You don't have the required supplies");
-			break;
-		case "JACKET":
-			if(searchInv("jacket")) {
-				wearingCoat = true;
-				System.out.println("You are now wearing the coat");
-			}
-		break; */
 		default:
 			System.out.println("Sorry, I don't recognize this command");
 			break;
@@ -190,6 +189,7 @@ public class AdventureMain {
 	void moveToRoom(char c) {
 		String nextRoomName = currentRoom.getExit(c);
 
+		//Checks if direction inputted is valid
 		if(nextRoomName.equals("")) {
 			System.out.println("You can't go there");
 			return;			
@@ -217,6 +217,7 @@ public class AdventureMain {
 			return;
 		} 
 
+		//If all checks have been passed, reset current room
 		currentRoom = roomList.get(nextRoomName);
 
 		//All puzzles taking effect in the room
@@ -239,6 +240,7 @@ public class AdventureMain {
 	}
 
 	boolean searchInv( String s) {
+		//Searches inventory if it contains item
 		for (Item item : invList) {
 			if (item.itemName.equals(s))  return true;
 			if (item.itemName.equals("jacket")) {
@@ -268,6 +270,7 @@ public class AdventureMain {
 	
 
 	void eatItem(String food) {
+		//Eat command
 		for(Item item: invList) {
 			if(item.itemName.equals(food.toLowerCase())){
 				if(item.edible) {
@@ -292,28 +295,33 @@ public class AdventureMain {
 	}
 
 	void readItem(String text) {
+		//Read command
+		String itemRead = text.toLowerCase();
 		for(Item item: invList) {
-			if(item.itemName.equals(text.toLowerCase())){
+			if(item.itemName.equals(itemRead)){
 				if(item.canRead) {
-					if(text.equals("researchpaper")) {
-						System.out.println("research paper text");
+					if(itemRead.equals("researchpaper")) {
+						System.out.println("From the paper, you summarize that the researchers had been testing with oil distillation\n"
+								  +"The oil extraction and distillation site, located at the Dig Site, had been used for multiple purposes."
+								  +"For example, allowing the researchers to produce their own fuel among various other purposes");
 					} 
-					if(text.equals("note")) {
+					if(itemRead.equals("note")) {
 						System.out.println("The note reads: It has awoken from its sleep. The beast must slumber again.");
 					} 
-					else if (text.equals("artifact")) {
+					else if (itemRead.equals("artifact")) {
 						System.out.println("Your head hurts simply just trying to understand the mysterious language.");
 					}
-				}else {
-					System.out.println("You can't read that");
 				}
-			} else {
-				System.out.println("you don't have that");
+				else {
+					System.out.println("You can't read that");
+					return;
+				}
 			}
 		}
 	}
 
 	void dropItem(String object) {
+		//Drops items from inventory list
 		for (Item item: invList) {
 			if(item.itemName.equals(object.toLowerCase())) {
 				item.location = currentRoom.getTitle();
@@ -323,6 +331,7 @@ public class AdventureMain {
 	}
 
 	void heliPuzzle() {
+		//Final helicopter puzzle for the player to escape
 		if (frozenPipes) System.out.println("The helicopter's pipes are frozen. Perhaps you could melt it");
 		if (needFuel) System.out.println("The engine needs fuel");
 		if (!artifactInPlace) System.out.println("There is a gap inside the instrument panel. The missing piece seems instrumental to the functioning of the helicopter");
@@ -330,6 +339,7 @@ public class AdventureMain {
 	}
 
 	void leave() {
+		//Allows player to leave if they have completed all helicopter sub-puzzles
 		if (!needFuel && !frozenPipes && artifactInPlace) {
 			System.out.println("Using your dormant power of planar manipulation, you successfully travel behind the universal curtain.\n"
 					+"You successfully end up on a sunny beach somewhere in what looks like Hawaii");
